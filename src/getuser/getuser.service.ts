def88d0ed507eda9b508278c071fd08956013e28
talafarael <127351345+talafarael@users.noreply.github.com>
2024-04-14 16:00:57 +0300

    getuser

--- src/getuser/getuser.service.ts
+++ src/getuser/getuser.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import {GetuserDto} from './getuser.dto'
+import * as jwt from 'jsonwebtoken';
 import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class GetuserService {
   constructor(private prisma: PrismaService) {}
-  async getuser(dto: GetuserDto) {}
+  async getuser(dto: GetuserDto) {
+    if (!dto.token) {
+		
+      throw new Error( "Пользователь не авторизован")
+		}
+
+    const decodedData = jwt.verify(dto.token,process.env.SECRET);
+   const id = decodedData.id;
+   const User = await this.prisma.users.findFirst({
+    where:{
+     id: id.trim()
+  }
+  });
+   if (!User) {
+    throw new Error( "Пользователь не авторизован")
+   }
+   return User
+  }
 }
